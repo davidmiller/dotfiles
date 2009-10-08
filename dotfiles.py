6f411efd346ac9a91452fd9dbc36152a55c2226c
David Miller <il.livid.dream@gmail.com>
2009-10-08 19:43:58 +0100

    split init into functions

--- dotfiles.py
+++ dotfiles.py
@@ -2,6 +2,7 @@
 """ Syncs dotfiles across machines """
 import logging
 import os
+import sqlite3
 import subprocess
 import StringIO
 import argparse
@@ -11,13 +12,27 @@ class Dotfiles:
 
 
     def init( self ):
+        """ Initialise the dotfiles dir, repo, db """
+        self.mk_dotfiles_dir()
+        self.mk_repo()
+
+
+    def mk_dotfiles_dir( self ):
+        """ Creates the .dotfiles dir """
         os.mkdir( self.dotfiles_dir )
+        return os.path.isdir( self.dotfiles_dir )
+
+
+    def mk_repo( self ):
+        """ Creates the dotfiles git repo """        
         self.cwd = os.getcwd()
         os.chdir( self.dotfiles_dir )
         gitout = StringIO.StringIO()
         git_init_proc = subprocess.Popen( ['git', 'init'], stdout = subprocess.PIPE )
         init_logger.debug( git_init_proc.stdout.read() )
+        return os.path.isdir( self.repo_dir )
 
+    
     def add( self ):
         """ Start tracking a dotfile """
         add_logger.debug( 'Adding ' + self.args.file )
@@ -34,11 +49,12 @@ class Dotfiles:
 
 
     def __init__( self, args=None ):
+        """ Constructs the dotfiles Class """
         self.args = args
         home = os.environ['HOME']
         dotfiles_dir = os.path.join( home, '.dotfiles' )
         self.dotfiles_dir = dotfiles_dir
-
+        self.repo_dir = os.path.join( self.dotfiles_dir, '.git' )
         if self.args.func:            
             fn = getattr( self, args.func )
             fn()
@@ -55,15 +71,21 @@ if __name__ == '__main__':
                          help='Turn on debugging notices')
     subparsers = parser.add_subparsers( help = 'sub command help' )
 
-    parser_sync = subparsers.add_parser( 'sync',
-                                         help = 'manually sync dotfiles' )
-    parser_sync.set_defaults( func = 'sync' )
+    # dotfiles init commands
+    parser_init = subparsers.add_parser( 'init', help = 'Initialise dotfiles' )
+    parser_init.set_defaults( func = 'init' )
 
+    # Adding dotfiles commands
     parser_add = subparsers.add_parser( 'add', help = 'Start tracking a dotfile' )
     parser_add.add_argument( 'file',
                              help='Location of the flie to track' )
     parser_add.set_defaults( func = 'add' )
-    
+
+    # Syncing dotfiles commands
+    parser_sync = subparsers.add_parser( 'sync',
+                                         help = 'manually sync dotfiles' )
+    parser_sync.set_defaults( func = 'sync' )
+
     args = parser.parse_args()
 
 
@@ -100,5 +122,5 @@ if __name__ == '__main__':
     add_logger.addHandler( console_handler )    
     sync_logger.addHandler( console_handler )
 
-
+    #Initialise the object
     dotfiles = Dotfiles( args )
